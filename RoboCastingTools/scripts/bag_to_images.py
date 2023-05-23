ba39d2c392db0909ccc1c95f6422d98af353030e
Rooholla-Khorrambakht <rk4342@nyu.edu>
2023-05-23 16:06:02 -0400

    package updated

--- RoboCastingTools/scripts/bag_to_images.py
+++ RoboCastingTools/scripts/bag_to_images.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-
 """Extract images from a rosbag.
 """
 
 import argparse
-from RoboCasterTools.rosbag_utils import *
+from RoboCastingTools.rosbag_utils import *
+
 
 def main():
-    """Extract a folder of images from a rosbag.
-    """
+    """Extract a folder of images from a rosbag."""
     parser = argparse.ArgumentParser(description="Extract images from a ROS bag.")
     parser.add_argument("bag_file", help="Input ROS bag.")
     parser.add_argument("output_dir", help="Output directory.")
@@ -19,5 +18,6 @@ def main():
 
     return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
